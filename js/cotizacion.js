8e1b81041d0567813b5f241503bde57da1505b33
Tomás Ledesma <tomasld13@gmail.com>
2023-11-15 21:52:01 -0300

    Se agrega funcionalidad de cotizaciones

---
+++ js/cotizacion.js
@@ -0,0 +1,45 @@
+const main = async () => {
+    const fetchURL = async (url) => {
+      const res = await fetch(`https://www.dolarsi.com/api/api.php?type=${url}`)
+      return res.json()
+    }
+
+    const cotizacionesDolar = await fetchURL("dolar");
+    console.log(cotizacionesDolar)
+    const crearHtmlCotizacionDolar = () => {
+      return cotizacionesDolar.map(cot => {
+        return `
+            <div class="cotizacion" style="background-color: #F6F1EE;">
+                <h5 class="location" id="location">${cot.casa.nombre}</h5>
+                <div class="forecast-details-container">
+                    <p>Compra: $${cot.casa.compra}</p>
+                    <p>Venta: $${cot.casa.compra}</p>
+                </div>
+            </div>
+        `
+      })
+    }
+    const cotizacionMainDolar = document.getElementById('cotizacion-main-dolar')
+    crearHtmlCotizacionDolar().forEach(cot => cotizacionMainDolar.appendChild(createElementFromHTML(cot)))
+  
+    const cotizacionesOtros = await fetchURL("cotizador");
+    console.log(cotizacionesOtros)
+    const crearHtmlCotizacionOtros = () => {
+      return cotizacionesOtros.map(cot => {
+        return `
+            <div class="cotizacion" style="background-color: #F6F1EE;">
+                <h5 class="location" id="location">${cot.casa.nombre}</h5>
+                <div class="forecast-details-container">
+                    <p>Compra: $${cot.casa.compra}</p>
+                    <p>Venta: $${cot.casa.compra}</p>
+                </div>
+            </div>
+        `
+      })
+    }
+    const cotizacionMainOtros = document.getElementById('cotizacion-main-otros')
+    crearHtmlCotizacionOtros().forEach(cot => cotizacionMainOtros.appendChild(createElementFromHTML(cot)))
+
+}
+
+document.addEventListener("DOMContentLoaded", main)
